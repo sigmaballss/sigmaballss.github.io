5f7b15bf377e51908d1170a307e3d0ebce9a087a
Антон Деревнин <anton.derevnin@itl.team>
2025-06-05 12:07:57 +0300

    Little changes

---
+++ SigmaBallssGitHubPage.Blazor/Components/SectionComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SigmaBallssGitHubPage.Blazor.Components;
+
+public partial class SectionComponent : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await Task.Delay(1000);
+        SectionClass = "focused-section";
+    }
+
+    private string SectionClass { get; set; } = string.Empty;
+}
---
+++ SigmaBallssGitHubPage.Blazor/Components/SectionComponent.razor.js
@@ -0,0 +1,5 @@
+﻿export class Section {
+  
+}
+
+window.Section = Section;
