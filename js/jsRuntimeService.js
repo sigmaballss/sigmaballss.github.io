31f415b85ae5aee8a1e7458f422eb3a277ae1cc4
sigmaballss <fff278473@gmail.com>
2025-06-05 12:48:48 +0000

    Deploying to gh-pages from @ sigmaballss/sigmaballss.github.io@b07fb3c47d2ee321bbf954c9fc6f710f1eb57c32 🚀

--- js/jsRuntimeService.js
+++ js/jsRuntimeService.js
@@ -1,8 +1,12 @@
 ﻿window.onscroll = function() {
     if (window.jsRuntimeService != null)
         window.jsRuntimeService.invokeMethodAsync('OnScroll', window.scrollY);
-}
+};
 
 window.RegisterJsRuntimeService = (jsRuntimeService) => {
     window.jsRuntimeService = jsRuntimeService;
-}
+};
+
+window.MyDOMGetBoundingClientRect = (element) => element.getBoundingClientRect().top;
+
+window.GetElementBoundingClientRect = (element) => element.getBoundingClientRect();
