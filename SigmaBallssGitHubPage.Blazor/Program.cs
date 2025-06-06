f754be9a71096a777366b8a93a7f7e262e72086a
Антон Деревнин <anton.derevnin@itl.team>
2025-06-06 17:49:08 +0300

    Updated WebAssemblyCultureResourcesHelper

--- SigmaBallssGitHubPage.Blazor/Program.cs
+++ SigmaBallssGitHubPage.Blazor/Program.cs
@@ -1,10 +1,9 @@
-using System.Globalization;
-using System.Reflection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SigmaBallssGitHubPage.Blazor;
 using SigmaBallssGitHubPage.Blazor.Services.Abstractions;
 using SigmaBallssGitHubPage.Blazor.Services.Impl;
+using SigmaBallssGitHubPage.Common.Helpers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,29 +14,6 @@ builder.Services.AddLocalization();
 builder.Services.AddSingleton<IJsRuntimeService, JsRuntimeService>();
 builder.Services.AddSingleton<ICultureProvider, CultureProvider>();
 
-#region Localization resources fix
-
-// Get current localization culture
-var currentCulture = CultureInfo.DefaultThreadCurrentCulture;
-
-// Get WASM culture provider via reflection
-var type = Assembly.GetAssembly(typeof(WebAssemblyHost))!.GetType("Microsoft.AspNetCore.Components.WebAssembly.Hosting.WebAssemblyCultureProvider");
-var instance  = type
-    !.GetProperty("Instance", BindingFlags.Public | BindingFlags.Static)
-    ?.GetValue(null);
-
-var cultureInfo = CultureInfo.CreateSpecificCulture("ru");
-
-// Swap out the "current culture" for the UI (localization) culture
-CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-// Load the satellite assemblies
-await (ValueTask)instance!
-    .GetType()
-    .GetMethod("LoadCurrentCultureResourcesAsync", BindingFlags.Public | BindingFlags.Instance)!
-    .Invoke(instance, [])!;
-// Swap the culture back
-CultureInfo.DefaultThreadCurrentCulture = currentCulture;
-
-#endregion
+await WebAssemblyCultureResourcesHelper.LoadResourcesForCultures(["ru", "en"]);
 
 await builder.Build().RunAsync();
---
+++ SigmaBallssGitHubPage.Common/Helpers/WebAssemblyCultureResourcesHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Reflection;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace SigmaBallssGitHubPage.Common.Helpers;
+
+public static class WebAssemblyCultureResourcesHelper
+{
+    public static async Task LoadResourcesForCultures(string[] cultureNames)
+    {
+        var cultureToRollback = CultureInfo.DefaultThreadCurrentCulture;
+
+        var type = Assembly.GetAssembly(typeof(WebAssemblyHost))!.GetType(
+            "Microsoft.AspNetCore.Components.WebAssembly.Hosting.WebAssemblyCultureProvider");
+
+        var webAssemblyCultureProvider = type!
+            .GetProperty("Instance", BindingFlags.Public | BindingFlags.Static)
+            ?.GetValue(null);
+
+        foreach (var cultureName in cultureNames)
+        {
+            var cultureInfo = CultureInfo.CreateSpecificCulture(cultureName);
+
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+
+            await (ValueTask)webAssemblyCultureProvider!
+                .GetType()
+                .GetMethod("LoadCurrentCultureResourcesAsync", BindingFlags.Public | BindingFlags.Instance)!
+                .Invoke(webAssemblyCultureProvider, [])!;
+        }
+
+        CultureInfo.DefaultThreadCurrentCulture = cultureToRollback;
+    }
+}
